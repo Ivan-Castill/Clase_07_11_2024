b9d589809d69d5aac195d95dbab00da033cbea09
Ivan-Castill <ivan2003castillo12345@gmail.comzx>
2024-11-07 08:34:45 -0500

    Clase 1

--- src/Converciones/Main.java
+++ src/Converciones/Main.java
@@ -1,7 +1,55 @@
-package introduccion;
-import
+package Converciones;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Introduce");
+
+
+        Scanner ingreso=new Scanner(System.in);
+        String val1;
+        double con_val1;
+
+        System.out.println("Ingrese el valor a convertir: ");
+        val1=ingreso.nextLine();
+        con_val1=Double.parseDouble(val1);
+
+        System.out.print("El resultado del valor convetido a double es: "+con_val1);
+
+
+        //Scanner ingreso=new Scanner(System.in);
+        //int val1;
+        //int result;
+        //String con_val1;
+
+        //System.out.println("Ingrese el valor a convertir: ");
+        //val1=ingreso.nextInt();
+        //con_val1=Integer.toString(val1);
+
+        //System.out.print("El resultado del valor convetido a string es: "+con_val1);
+
+
+        //Scanner ingreso=new Scanner(System.in);
+        //double val1;
+        //int result;
+        //int con_val1;
+
+        //System.out.println("Ingrese el valor a multiplicar: ");
+        //val1=ingreso.nextDouble();
+        //con_val1=(int)(val1);
+        //result=con_val1*2;
+
+        //System.out.print("El resultado "+con_val1+" * 2 es "+result);
+
+
+        //Scanner ingreso=new Scanner(System.in);
+        //String val1;
+        //int result;
+        //int con_val1;
+
+        //System.out.println("Ingrese el valor a multiplicar: ");
+        //val1=ingreso.nextLine();
+        //con_val1=Integer.parseInt(val1);
+        //result=con_val1*2;
+
+        //System.out.print("El resultado "+con_val1+" * 2 es "+result);
     }
 }
